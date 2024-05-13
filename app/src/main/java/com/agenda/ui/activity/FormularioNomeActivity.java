4a32ca039f678a76a60274ec80a8dedac157b59f
Matheus <matheushellf@gmail.com>
2024-05-13 10:09:03 -0300

    refatoração em FormularioNomeActivity, ListaNomesActivity

--- app/src/main/java/com/agenda/ui/activity/FormularioNomeActivity.java
+++ app/src/main/java/com/agenda/ui/activity/FormularioNomeActivity.java
@@ -2,45 +2,66 @@ package com.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.agenda.R;
 import com.agenda.dao.AlunoDAO;
 import com.agenda.model.Aluno;
 
+import java.io.Serializable;
+
 public class FormularioNomeActivity extends AppCompatActivity {
+
+    private EditText campoNome;
+    private EditText campoTelefone;
+    private EditText campoEmail;
+    private final AlunoDAO dao = new AlunoDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_nome);
+        setTitle("Novo contato");
+        inicializacaoCampos();
+        configuraBotaoSalvar();
 
-        final AlunoDAO dao = new AlunoDAO();
+        Intent dados = getIntent();
+        Aluno aluno = (Aluno) dados.getSerializableExtra("nome");
+        campoNome.setText(aluno.getNome());
+        campoTelefone.setText(aluno.getTelefone());
+        campoEmail.setText(aluno.getEmail());
+    }
 
-        final EditText campoNome = findViewById(R.id.lista_campo_nome);
-        final EditText campoTelefone = findViewById(R.id.lista_campo_telefone);
-        final EditText campoEmail = findViewById(R.id.lista_campo_email);
-
-
+    private void configuraBotaoSalvar() {
         Button botaoSalvar = findViewById(R.id.lista_button_salvar);//bind da view do botao salvar
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nome = campoNome.getText().toString();
-                String telefone = campoTelefone.getText().toString();
-                String email = campoEmail.getText().toString();
-
-                Aluno alunoCriado = new Aluno(nome, telefone, email);
-                dao.salva(alunoCriado);
-
-                startActivity(new Intent(FormularioNomeActivity.this, ListaNomesActivity.class));
-
-            }
+        botaoSalvar.setOnClickListener(v -> {
+            startActivity(new Intent(FormularioNomeActivity.this, ListaNomesActivity.class));
+            Aluno alunoCriado = criaAluno();
+            salva(alunoCriado);
         });
+    }
 
+    private void inicializacaoCampos() {
+        campoNome = findViewById(R.id.lista_campo_nome);
+        campoTelefone = findViewById(R.id.lista_campo_telefone);
+        campoEmail = findViewById(R.id.lista_campo_email);
+    }
 
+    private void salva(Aluno aluno) {
+        dao.salva(aluno);
+        finish();
+    }
 
+    @NonNull
+    private Aluno criaAluno() {
+        String nome = campoNome.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        return new Aluno(nome, telefone, email);
     }
 }
--- app/src/main/java/com/agenda/ui/activity/ListaNomesActivity.java
+++ app/src/main/java/com/agenda/ui/activity/ListaNomesActivity.java
@@ -3,35 +3,56 @@ package com.agenda.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.agenda.R;
 import com.agenda.dao.AlunoDAO;
+import com.agenda.model.Aluno;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 public class ListaNomesActivity extends AppCompatActivity {
+        private final AlunoDAO dao = new AlunoDAO();
 // quando colocamos o AppCompatActivity, ele vai manter a App bar com o título do nosso projeto, que no caso é "Agenda".
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Agenda");
         setContentView(R.layout.activity_lista_nomes);
+        configuraFabNovoAluno();
+        dao.salva(new Aluno("Alex", "1122223333", "alex@alura.com.br"));
 
+    }
 
+    private void configuraFabNovoAluno() {
         FloatingActionButton botaoNovoAluno = findViewById(R.id.lista_fab_novo_aluno);//primeiro é feito o bind do FLOATING BUTTON
         botaoNovoAluno.setOnClickListener(v -> { //depois o click para escutar o floating butao para ir ao formulario
             startActivity(new Intent(ListaNomesActivity.this, FormularioNomeActivity.class));//referencia da activity que voce esta, activity que voce quer ir(no caso lista de nomes, indo para formulario
-            finish(); // finaliza a activity de formulario e volta para a anterior
+            finish();
         });
     }
+
     @Override
     protected void onResume(){
         super.onResume();
+        configuraLista();
+    }
 
-        AlunoDAO dao = new AlunoDAO();
+    private void configuraLista() {
         ListView listaNomes = findViewById(R.id.lista_nomes); //bind da list view
-        listaNomes.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos())); //implementando adapter na listview
+        List<Aluno> alunos = dao.todos();
+        listaNomes.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos)); //implementando adapter na listview
 
+        listaNomes.setOnItemClickListener((parent, view, posicao, id) -> {
+            Aluno alunoEscolhido = alunos.get(posicao); //pega o aluno de acordo com sua posicao
+            Intent vaiParaFormularioActivity = new Intent(ListaNomesActivity.this, FormularioNomeActivity.class);
+            vaiParaFormularioActivity.putExtra("nome",alunoEscolhido); // para mandar DADOS VIA EXTRA ele precisa ser "serializable"
+            startActivity(vaiParaFormularioActivity);
+        });
     }
 }
